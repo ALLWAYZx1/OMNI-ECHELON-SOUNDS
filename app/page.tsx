905c98b8505fcf94327c5b878c86ad7ca0bb483d
ALLWAYZx1 <te3bird22@gmail.com>
2026-04-29 02:27:26 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -139,7 +139,7 @@ export default function OmniEchelonSoundsWebsite() {
       frameBorder="no"
       allow="autoplay"
       className="rounded-2xl"
-      src="https://w.soundcloud.com/player/?url=https://soundcloud.com/allwayzinthemixx/toxic_city_freestyle&auto_play=false&show_artwork=false&show_comments=false&show_user=true&show_playcount=false&sharing=false"
+       src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2309145506&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
     ></iframe>
   </div>
 </div>
