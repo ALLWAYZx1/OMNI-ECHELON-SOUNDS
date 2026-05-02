2c18beb02f6ff8eeab5be3c851ed74d8ea0f2bd2
ALLWAYZx1 <te3bird22@gmail.com>
2026-05-02 16:22:52 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -143,6 +143,7 @@ export default function OmniEchelonSoundsWebsite() {
     src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2313545609&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
   />
 </div>
+    </div>
           </div>
         </section>
 
