0d868d0f5822ecc313d5f268ce2fcc234adc355a
ALLWAYZx1 <te3bird22@gmail.com>
2026-05-02 16:16:07 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -132,15 +132,16 @@ export default function OmniEchelonSoundsWebsite() {
 
     
 
-    <iframe
-      width="100%"
-      height="166"
-      scrolling="no"
-      frameBorder="no"
-      allow="autoplay"
-      className="rounded-2xl"
-<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2313545609&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/allwayzinthemixx" title="ALLWAYZintheMIXX" target="_blank" style="color: #cccccc; text-decoration: none;">ALLWAYZintheMIXX</a> · <a href="https://soundcloud.com/allwayzinthemixx/destroy-everything-pre-release" title="DESTROY EVERYTHING (Pre-Release)" target="_blank" style="color: #cccccc; text-decoration: none;">DESTROY EVERYTHING (Pre-Release)</a></div>    ></iframe>
-  </div>
+    <div className="overflow-hidden rounded-2xl border border-white/10 bg-white/5">
+  <iframe
+    width="100%"
+    height="166"
+    scrolling="no"
+    frameBorder="no"
+    allow="autoplay"
+    className="rounded-2xl"
+    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2313545609&color=%23ff5500"
+  />
 </div>
             </div>
           </div>
