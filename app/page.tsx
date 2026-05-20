d24f4bbb05a20c01b77107356564b07db96fd979
ALLWAYZx1 <te3bird22@gmail.com>
2026-05-19 20:06:19 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -147,7 +147,7 @@ export default function OmniEchelonSoundsWebsite() {
   frameBorder="no"
   allow="autoplay"
   className="rounded-2xl"
-  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2319986123&color=%2327150b&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2322414041&color=%23d88c4a&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
 ></iframe>
         </div>
 
