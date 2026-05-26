d0f312dfecc9b40f80c6e2141813581e2e104f1e
ALLWAYZx1 <te3bird22@gmail.com>
2026-05-25 23:38:29 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -131,7 +131,12 @@ export default function OmniEchelonSoundsWebsite() {
 
           {/* SPOTIFY */}
           <div className="mb-4 overflow-hidden rounded-2xl border border-white/10 bg-white/5">
-           <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/7lu8CK1gQztFv3rlB0hqsb?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe
+              src="https://open.spotify.com/embed/album/7AKc4hD4Z57wgl24hSOKzQ?utm_source=generator&theme=0"
+              width="100%"
+              height="152"
+              allow="autoplay; clipboard-write; encrypted-media"
+            ></iframe>
           </div>
 
           {/* SOUNDCLOUD */}
