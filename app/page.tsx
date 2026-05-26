f4f126edcc7aab16c98cc09e6d83b3aa68bdc4f6
ALLWAYZx1 <te3bird22@gmail.com>
2026-05-25 23:35:46 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -132,7 +132,7 @@ export default function OmniEchelonSoundsWebsite() {
           {/* SPOTIFY */}
           <div className="mb-4 overflow-hidden rounded-2xl border border-white/10 bg-white/5">
             <iframe
-              src="https://open.spotify.com/embed/artist/7lu8CK1gQztFv3rlB0hqsb?utm_source=generator"0"
+              src="https://open.spotify.com/embed/artist/7lu8CK1gQztFv3rlB0hqsb?utm_source=generator" width="100%" height="352" frameBorder="0"
               width="100%"
               height="152"
               allow="autoplay; clipboard-write; encrypted-media"
