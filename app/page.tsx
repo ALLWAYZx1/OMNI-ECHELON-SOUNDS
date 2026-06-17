5965df795b48a0dda4f0043f11751e61088948a4
ALLWAYZ <te3bird22@gmail.com>
2026-06-16 17:30:16 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -188,7 +188,7 @@ export default function OmniEchelonSoundsWebsite() {
   <div className="relative w-full" style={{ paddingBottom: "56.25%" }}>
     <iframe
       className="absolute inset-0 h-full w-full"
-      src="https://youtu.be/rfip6Ix-SgA?si=mjj71yKWLnTa3rPd"
+      src="https://youtu.be/rfip6Ix-SgA"
       title="Transmission"
       allowFullScreen
     />
